737f3df7ed9970f5cc2f129b179f06fdf8f9b667
zhangfs <zhangfs@bjtu.edu.cn>
2022-04-11 21:52:59 +0800

    Hot-fix first commit

--- src/main/java/com/zfs/ssm/test/DaoTest.java
+++ src/main/java/com/zfs/ssm/test/DaoTest.java
@@ -4,6 +4,7 @@ import com.zfs.ssm.dao.DeptMapper;
 import com.zfs.ssm.dao.EmpMapper;
 import com.zfs.ssm.pojo.Dept;
 import com.zfs.ssm.pojo.Emp;
+import com.zfs.ssm.service.EmpService;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,13 +31,11 @@ public class DaoTest {
     EmpMapper empMapper;
     @Autowired
     SqlSession sqlSession;
+    @Autowired
+    EmpService empService;
     @Test
     public void testCRUD(){
-        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-        for (int i = 0; i < 1000; i++) {
-            String uid = UUID.randomUUID().toString().substring(0, 5)+i;
-            mapper.insertSelective(new Emp(null,uid,"男",uid+"@qq.com",1));
-        }
-        System.out.println("完成了！");
+        List<Emp> emps = empService.getAll();
+        emps.forEach(emp -> System.out.println(emp));
     }
 }
